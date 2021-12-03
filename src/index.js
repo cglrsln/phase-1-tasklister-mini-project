4b6cfd135d6aaf84e3a931f71a0ab114e003df5c
Caglar <caglarsulun@hotmail.com>
2021-12-02 22:01:19 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form")
+  
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //console.log(e.target.newTaskDescription.value)
+    // I changed new-task-description to newTaskDescription in index.html (id="newTaskDescription")
+    //<input type="text" id="newTaskDescription" name="new-task-description" placeholder="description">
+    buildToDo(e.target.newTaskDescription.value)
+    
+    form.reset()
+  })
 });
+
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#tasks").appendChild(p)
+  }
+
+
+  function handleDelete(e){
+      e.target.parentNode.remove()
+  }
